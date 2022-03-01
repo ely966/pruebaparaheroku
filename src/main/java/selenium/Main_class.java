2dd59231b15f99ddca808fbde9a89801c493f85d
ely966 <46519177+ely966@users.noreply.github.com>
2022-03-01 11:04:11 +0100

    first commit

--- src/main/java/selenium/Main_class.java
+++ src/main/java/selenium/Main_class.java
@@ -1,1 +1,46 @@
 
+package selenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main_class {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		//try {
+		//	driver.get("https://www.wikipedia.es");
+		//	Thread.sleep(5000);	
+		//}catch (InterruptedException e) {
+		//	e.printStackTrace();
+		//}finally {
+		//driver.quit();
+		//}
+		try {
+			driver.get("http://www.localhost:9000/portada");
+			
+			WebElement usernameBox= driver.findElement(By.id("username"));
+			WebElement passBox= driver.findElement(By.id("passw"));
+
+			WebElement usernameBoton= driver.findElement(By.id("enviar"));
+			//tiempo d espera apra verlo
+			Thread.sleep(6000);
+			//enviar texto
+			usernameBox.sendKeys("maria");
+			passBox.sendKeys("maria");
+			Thread.sleep(6000);
+			//pulsar el boton
+			usernameBoton.click();
+			
+			
+		}catch (InterruptedException e) {
+				e.printStackTrace();
+		}finally {
+			//driver.quit();
+		}
+
+	}
+
+}
